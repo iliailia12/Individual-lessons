e1743f2e6a0ce9ea6ee041bf3b495c246ad89157
iliailia12 <iliakviciani392@gmail.com>
2026-06-19 01:23:46 -0700

    second commit

--- .history/Day 099/Homework/task3/task3_20260619011850.js
+++ .history/Day 099/Homework/task3/task3_20260619011850.js
@@ -1,17 +1,12 @@
 // 3) შექმენი ვებ გვერდი სადაც გექნება 1 დივ ბლოკი და 2 ღილაკი, პირველ ღილაკზე დაკლიკების შემდეგ დივ ბლოკის background color - ი უნდა შეიცვალოს random ფერზე (შექმენი მასივი 5 ფერით და ყოველ ჯერზე სხვადასხვა ფერის უნდა იყოს დივის background color - ი), მეორე ღილაკზე დაკლიკებისას div - ის background color - ი უნდა გახდეს წითელი. (აუცილებლად უნდა იყოს გამოყენებული addEventListener - ი)
 
 
-const containerDiv = document.getElementsByTagName('div')[0]; 
-const randomColors = document.getElementById('randomColors');
-const resetColorButton = document.getElementById('red');
+const div = document.getElementsByTagName('div')
+const randomColors = document.getElementById('randomColors')
+const red = document.getElementById('red')
 
-randomColors.addEventListener('click', () => {
-    const colors = ['red', 'blue', 'grey', 'black', 'purple'];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-
-    containerDiv.style.backgroundColor = colors[randomIndex];
-});
-
-resetColorButton.addEventListener('click', () => {
-    containerDiv.style.backgroundColor = 'red';
-});
+randomColors.addEventListener('click',()=>{
+                   // 0     1     2      3       4
+    const colors = ['red','blue','grey','black','purple']
+    const randomIndex = Math.floor(Math.random() * colors.length)
+})
--- .history/Day 099/Homework/task3/task3_20260619012322.js
+++ .history/Day 099/Homework/task3/task3_20260619012322.js
@@ -1,7 +1,7 @@
 // 3) შექმენი ვებ გვერდი სადაც გექნება 1 დივ ბლოკი და 2 ღილაკი, პირველ ღილაკზე დაკლიკების შემდეგ დივ ბლოკის background color - ი უნდა შეიცვალოს random ფერზე (შექმენი მასივი 5 ფერით და ყოველ ჯერზე სხვადასხვა ფერის უნდა იყოს დივის background color - ი), მეორე ღილაკზე დაკლიკებისას div - ის background color - ი უნდა გახდეს წითელი. (აუცილებლად უნდა იყოს გამოყენებული addEventListener - ი)
 
 
-const containerDiv = document.getElementsByTagName('div')[0]; 
+const containerDiv = document.getElementsByTagName('div')[0]; // first div
 const randomColors = document.getElementById('randomColors');
 const resetColorButton = document.getElementById('red');
 
